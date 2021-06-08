24eec454abb85946653724bb877521ea02af2d01
T-70ya <ahamo109docomo@gmail.com>
2021-06-08 17:09:17 +0900

    新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'contents/index'
 
   root to: 'contents#index'
-  resources :contents
+  resources :contents, only: [:index, :create, :show, :edit, :destroy, :update]
 end
