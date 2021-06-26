38315f7a163f9527cf86024de44164da8f7b5c77
T-70ya <ahamo109docomo@gmail.com>
2021-06-26 17:31:19 +0900

    Basic認証非表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  #before_action :basic_auth
   before_action :authenticate_user!
   before_action :sinkitoroku, if: :devise_controller?
 
   private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_NINSHO_USER"] && password == ENV["BASIC_NINSHO_PASSWORD"]
-    end
-  end
+  #def basic_auth
+    #authenticate_or_request_with_http_basic do |username, password|
+      #username == ENV["BASIC_NINSHO_USER"] && password == ENV["BASIC_NINSHO_PASSWORD"]
+    #end
+  #end
 
   def sinkitoroku
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
